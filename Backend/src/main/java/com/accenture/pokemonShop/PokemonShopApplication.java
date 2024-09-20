76bb25d4c4066bc94d068c8ef5e7dd70e9a18dba
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-09-20 11:01:49 -0400

    First Commit

---
+++ Backend/src/main/java/com/accenture/pokemonShop/PokemonShopApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.pokemonShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonShopApplication.class, args);
+	}
+
+}
