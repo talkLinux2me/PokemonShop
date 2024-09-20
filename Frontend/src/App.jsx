bda94938cbf9e011d0affe5a96bf80ae306427b2
Nasseima <lindsaynasseima@gmail.com>
2024-09-20 17:01:50 -0400

    created the admin page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
+<<<<<<< Updated upstream
 // import Fetching from './Fetching.jsx'
+=======
+import Admin from './components/Admin'
+>>>>>>> Stashed changes
 
 import Home from './componenta/Home.jsx'
 function App() {
+<<<<<<< Updated upstream
 
 
 
   return (
     <>
       <Home />
+=======
+  
+  return (
+    <>
+     <Admin/>
+>>>>>>> Stashed changes
     </>
   )
 }
