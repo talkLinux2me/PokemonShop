c176a755df87da372825737eb00301f21d369c4a
abdoulfataho <yamyaogofataho@gmail.com>
2024-09-20 12:26:56 -0400

    user model created

--- Backend/src/main/java/com/accenture/pokemonShop/users/model/Users.java
+++ Backend/src/main/java/com/accenture/pokemonShop/users/model/Users.java
@@ -1,4 +1,27 @@
 package com.accenture.pokemonShop.users.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String email;
+    private String phone_number;
+    private String role;
+    private String credit_card_info;
+    private String shipping_adress;
+    private String password;
 }
