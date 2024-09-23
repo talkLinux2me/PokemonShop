2d07709fefa87ffeccc6a0ff222777fccbd679a4
irashoemo1 <64241845+irashoemo1@users.noreply.github.com>
2024-09-23 09:09:20 -0400

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import SignUp from "./components/Signup"
-
+import Admin from "./components/Admin"
+import Home from "./components/Home.jsx"
+import "./App.css"
 
 function App() {
  
   return (
     <>
       <SignUp/>
+      <Home />
+      <Admin/>
     </>
   )
 }
